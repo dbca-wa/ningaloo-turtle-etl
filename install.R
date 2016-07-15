7ada7207761fecb0d95f8b0608a305d4d0478679
florianm <florian.mayer@dpaw.wa.gov.au>
2016-07-15 17:33:42 +0800

    added spatial modelling workbook

--- install.R
+++ install.R
@@ -5,3 +5,9 @@ system("sudo apt-get install mdbtools")
 install.packages(c("Hmisc", "dplyr", "lubridate", "sp", "rgdal", "mapview",
                    "DT","devtools"))
 devtools::install_github("ropensci/ckanr")
+
+# Install packfor from source
+tmp <- tempfile(tmpdir="data")
+download.file("http://download.r-forge.r-project.org/src/contrib/packfor_0.0-8.tar.gz", tmp)
+install.packages(tmp, repos = NULL, type = "source")
+unlink(tmp)
